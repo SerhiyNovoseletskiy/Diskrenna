cd1e4885fbafd211155f4ea1e28ec26a73ae5870
Сергей <novoseletskiyserhiy@gmail.com>
2015-05-13 18:58:40 +0300

    Зробив мінімізацію функції по кількості доданків

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-var number_of_vars = null;
+var number_of_vars = null; // На поле для введення кількості змінних
 
+// Коли наша сторінка уже загрузилась
 $(document).ready(function () {
+    // Отримуємо посилання на поле введення кількості змінних
     number_of_vars = $('#number_of_vars');
+    // Викликаємо функцію відмальовки таблиці
     draw_table();
 });
 
 // Функція для відмальовки таблиці
 function draw_table() {
+    // Малюємо шапку таблиці
     draw_header_of_table();
+    // Малюємо тіло таблиці
     draw_body_of_table();
 }
 
@@ -28,17 +33,16 @@ function draw_header_of_table() {
     thead.append("<td>F</td>");
 }
 
-
+// Відмальовка тіла таблиці
 function draw_body_of_table() {
-    // Кількість змінних
-    var number = parseInt(number_of_vars.val());
-    var html;
+    var number = parseInt(number_of_vars.val());   // Кількість змінних
+    var html; // Тут буде зберігатись наш html код
+    var count_of_rows = Math.pow(2, number); // Кількість рядків у таблиці
 
-
-    var count_of_rows = Math.pow(2, number);
-
-    // Тіло таблиці
+    // Отримуємо посилання на тіло таблиці
     var tbody = $('#table tbody');
+
+    // Очищення таблиці
     tbody.html('');
 
     for (var i = 1; i <= count_of_rows; i++) {
@@ -54,6 +58,7 @@ function draw_body_of_table() {
         tbody.append(html);
     }
 }
+
 function only(e) {
     var cols = $('#row_1 td');
     var rows = $('table tr');
@@ -136,8 +141,9 @@ function dovuznachenna_funkcii() {
     }
 }
 
-// Мінімізація функції
+// Мінімізація функції за кілкістю доданків
 function minimization() {
+    var list_of_struct = new Array();
 
     function minimize(f) {
         var temp;
@@ -147,6 +153,12 @@ function minimization() {
         var tmp = 'a';
         var k = 0;
         var a = new Array();
+
+        var struct = {
+            'length': 0,
+            'value': ''
+        };
+
 
         for (var i = 0; i < rows.length; i++) {
             console.log(f);
@@ -184,16 +196,36 @@ function minimization() {
         }
 
         console.log('Відображаю А');
-
         for (var i = 1; i < a.length; i++) {
             tmp = 'a';
             for (j = 0; j < a[i].indexes.length; j++) {
-                tmp += a[i].indexes[j];
+                tmp += a[i].indexes[j] + ' ';
+
+                if (a[i-1].value == 1)
+                    if (a[i].indexes.length == 1 && a[i].indexes[0] == 0) {
+                        struct.value += '0';
+                    } else {
+                        if (j == 0) {
+                            struct.value += 'X';
+                            struct.value += a[i].indexes[j]
+                        } else {
+                            struct.value += a[i].indexes[j]
+                        }
+                    }
             }
 
-            tmp += ' = ' + a[i-1].value;
+            tmp += ' = ' + a[i - 1].value;
             console.log(tmp);
+
+            if (a[i - 1].value == 1) {
+                struct.length++;
+                struct.value += '&theta;';
+            }
         }
+
+        console.log('Мінімізована функція: ' + struct.value);
+        console.log('Кількість доданків :' + struct.length);
+        list_of_struct.push(struct)
     }
 
     var f = $('#dovuznachenna_funkcii ul li').each(function () {
@@ -201,6 +233,18 @@ function minimization() {
         minimize($(this).html());
     });
 
+    var min = list_of_struct[0].length;
+    var value = list_of_struct[0].value;
+
+    for (var i = 1; i < list_of_struct.length; i++) {
+        if (min > list_of_struct[i].length) {
+            min = list_of_struct[i].length;
+            value = list_of_struct[i].value;
+        }
+    }
+
+    value = value.substr(0, value.lastIndexOf('&theta;'));
+    Materialize.toast(value);
 }
 
 
